f006e3bceae67b171501522a62a8c88d954e854f
RehnaHassan <127751244+RehnaHassan@users.noreply.github.com>
2023-09-21 15:16:04 +0530

    Create program3_exceptionhandling.py

---
+++ program3_exceptionhandling.py
@@ -0,0 +1,18 @@
+def is_valid_ip_address(ip):
+    try:
+        # Split the IP address into its components
+        components = ip.split('.')
+        
+        # Check that the IP address has exactly 4 components
+        if len(components) != 4:
+            return False
+        
+        # Check that each component is a valid integer between 0 and 255
+        for component in components:
+            value = int(component)
+            if value < 0 or value > 255:
+                return False
+        
+        return True
+    except ValueError:
+        return False
